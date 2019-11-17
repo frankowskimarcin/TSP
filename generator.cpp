3c9a0440024ee03c0ea259417578bf979092c1ea
frankowskimarcin <marcinfrankowski11@gmail.com>
2019-11-17 23:27:48 +0100

    Generator of instances

---
+++ generator.cpp
@@ -0,0 +1,17 @@
+#include "generator.h"
+#include <iostream>
+#include <fstream>
+#include <chrono>
+#define MAX 100
+
+void generuj() {
+	std::fstream plik;
+	srand(time(NULL));
+	plik.open("dane1.txt", std::ios::out | std::ios::trunc);
+	plik << MAX << std::endl;
+	for (int i = 1; i <= MAX; i++) {
+		plik << i << " ";
+		plik << rand() % (MAX * 100 + 1) << " ";
+		plik << rand() % (MAX * 100 + 1) << std::endl;
+	}
+}
