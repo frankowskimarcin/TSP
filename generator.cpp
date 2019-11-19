10e1c3731d563df9b8ecf03c55fe0e02bf947931
frankowskimarcin <marcinfrankowski11@gmail.com>
2019-11-19 22:53:02 +0100

    Instances generator

--- generator.cpp
+++ generator.cpp
@@ -8,10 +8,15 @@ void generuj() {
 	std::fstream plik;
 	srand(time(NULL));
 	plik.open("dane1.txt", std::ios::out | std::ios::trunc);
+	if (!plik.good()) {
+		std::cout << "Blad pliku" << std::endl;
+		exit(0);
+	}
 	plik << MAX << std::endl;
 	for (int i = 1; i <= MAX; i++) {
 		plik << i << " ";
 		plik << rand() % (MAX * 100 + 1) << " ";
 		plik << rand() % (MAX * 100 + 1) << std::endl;
 	}
+	plik.close();
 }
